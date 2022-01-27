bd26192721b65a924a53a6ffea46e81302e956ed
Soroush Syed <soroushasyed@gmail.com>
2022-01-27 11:59:08 -0500

    Added some more CLI commands to the commands.py file

--- commands.py
+++ commands.py
@@ -7,6 +7,16 @@ def command(name):
         return callback
     return inner
 
-@command("backup-config")
+@command("backup-running-config")
 def backup_config(switch, args):
     switch.backup_running_config(args.dest_path)
+
+@command("save-cmd-results")
+def save_cmd_results(switch, args):
+    if args.cmd is None:
+        raise ValueError("Must supply command through -c 'command' switch")
+    switch.save_cmd_results(args.cmd, args.dest_path)
+
+@command("scan-ports")
+def scan_ports(switch, args):
+    switch.scan_ports()
