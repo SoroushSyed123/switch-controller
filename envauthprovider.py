5ae955ab734caa5a5fe6a3f4f038cb96eb9683ba
Soroush Syed <soroushasyed@gmail.com>
2022-01-27 14:57:43 -0500

    Transformed the KeyError into a ValueError.

--- envauthprovider.py
+++ envauthprovider.py
@@ -5,4 +5,7 @@ class EnvAuthProvider(AuthProvider):
     def __init__(self, env):
         self.env = env
     def get_auth(self):
-        return (self.env["_USERNAME"], self.env["_PASSWORD"])
+        try:
+            return (self.env["_USERNAME"], self.env["_PASSWORD"])
+        except KeyError as error:
+            raise ValueError("missing required env") from error
